e69fc38a6f15f25029d958be14f7d5e303507793
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-26 15:53:15 -0300

    feat: implemented game page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import Image from 'next/image'
+import { Header } from './components/Header'
+import { KeyBoard } from './components/Keyboard'
+import { GameHeader } from './components/Game-header'
+import { GallowsPole } from './components/gallows/Gallows-pole'
+import { WordField } from './components/word/Word-field'
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hang Man Game</h1>
+    <main className="w-screen h-screen">
+
+      <div className="max-w-[430px] w-full h-auto bg-slate-900">
+
+        <Header />
+
+        <div className=" p-5 flex flex-col gap-6 justify-between">
+
+          <div className="w-full flex flex-col justify-between gap-10">
+            <GameHeader />
+            <GallowsPole />            
+            <WordField totalField={15} />            
+          </div>
+
+          <KeyBoard />
+          
+        </div>
+
+      </div>
     </main>
   )
 }
