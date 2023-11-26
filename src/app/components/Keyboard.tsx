4ee512dd0a5115b0d9cfedfb92e4ea7644cb61fd
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-26 15:50:42 -0300

    feat: implemented keyboard component

---
+++ src/app/components/Keyboard.tsx
@@ -0,0 +1,23 @@
+import { Key } from "./Key"
+
+const keyBoardLetters = [
+  'a', 'b', 'c', 'd', 'e',
+  'f', 'g', 'h', 'i', 'j',
+  'k', 'l', 'm', 'n', 'o',
+  'p', 'q', 'r', 's', 't',
+  'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+export const KeyBoard = () => {
+  return (
+    <div className="w-full h-auto bg-indigo-700 rounded-md p-2">
+      <ul className="w-full h-full flex flex-wrap gap-2 justify-center">
+        {keyBoardLetters.map((KeyboardLetter, index) => (
+          <li key={index} >
+            <Key value={KeyboardLetter} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
+}
