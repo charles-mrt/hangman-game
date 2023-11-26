0a13bff51dd53df9ca11c8b1bdc27cbfae9ca952
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-26 19:28:51 -0300

    feat: implement key clicked

--- src/app/components/Key.tsx
+++ src/app/components/Key.tsx
@@ -1,12 +1,19 @@
+'use client'
+
+import { useKeyContext } from "../context/Key-provider"
 
 interface KeyProps {
   value: string
 }
 
 export const Key = ({ value }: KeyProps) => {
+
+  const { handleKeyClick } = useKeyContext()
+
   return (
     <>
       <button 
+        onClick={()=>(handleKeyClick(value))}
         value={value}
         className="text-white text-2xl font-bold bg-indigo-500 rounded-md w-14 h-14 uppercase hover:bg-indigo-400 flex items-center justify-center"
       >
