dcbc07a7dc58ba17615823d6e75736efb1d54e78
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-26 15:50:01 -0300

    feat: implemented header component

---
+++ src/app/components/Header.tsx
@@ -0,0 +1,13 @@
+import { RefreshCw } from "lucide-react"
+
+
+export const Header = () => {
+  return (
+    <div className="w-full h-14 bg-slate-950 border-b-2 border-indigo-500 flex items-center justify-between px-4">
+      <h1 className="text-2xl font-bold text-indigo-500">
+        Hand Man Game
+      </h1>
+      <button className="text-white hover:text-indigo-500"><RefreshCw /></button>
+    </div>
+  )
+}
