01faaeeebfbfe40457bc543e57bf7c7c2136e500
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-26 16:34:28 -0300

    style: add background color and screen size

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`bg-slate-900 ${inter.className} w-screen h-screen overflow-hidden`}>{children}</body>
     </html>
   )
 }
