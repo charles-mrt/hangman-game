2a5b5defea3595aa3b87f448735bea6dc1f4537c
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-30 14:08:54 -0300

    feat: implement Rope color props

--- src/app/components/gallows/rope/Rope.tsx
+++ src/app/components/gallows/rope/Rope.tsx
@@ -1,14 +1,16 @@
 interface RopeProps {
   margin?:string
+  bg_color?: string
 }
 
-export const Rope = ({margin}:RopeProps) => {
+export const Rope = ({margin, bg_color}:RopeProps) => {
+  const background = bg_color === undefined ? 'bg-gray-800' : bg_color
   return (
     <>
-      <div className="w-1 h-1 bg-white rounded-full" />
-      <div className={`w-1 h-1 bg-white rounded-full ${margin}`} />
-      <div className={`w-1 h-1 bg-white rounded-full ${margin}`} />
-      <div className="w-1 h-1 bg-white rounded-full" />
+      <div className={`w-1 h-1 ${background} rounded-full`} />
+      <div className={`w-1 h-1 ${background} rounded-full ${margin}`} />
+      <div className={`w-1 h-1 ${background} rounded-full ${margin}`} />
+      <div className={`w-1 h-1 ${background} rounded-full`} />
     </>
   )
 }
