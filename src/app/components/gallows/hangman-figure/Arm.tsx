3c9fd7224e8dc191cab8827ee1f1932c61f982f1
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-30 10:26:49 -0300

    feat: Implement Arm component for hangman figure

--- src/app/components/gallows/hangman-figure/Arm.tsx
+++ src/app/components/gallows/hangman-figure/Arm.tsx
@@ -1,5 +1,12 @@
-export const Arm = () => {
+interface ArmProps {
+  is_border_color?: boolean
+}
+
+export const Arm = ({ is_border_color }: ArmProps) => {
   return (
-    <div className="w-2 h-10 mt-1 rounded-md border-2 border-indigo-800" />
+    <div className={`w-2 h-10 mt-1 rounded-md border-2 ${is_border_color  ? 'border-red-500' : 'border-gray-800'}`}/>
   )
 }
+
+
+
