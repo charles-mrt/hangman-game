678f3aa9fa98dcadc982290f8e2b49b981266555
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-30 10:23:44 -0300

    feat: Implement GameHeader component to display score and remaining attempts

--- src/app/components/Game-header.tsx
+++ src/app/components/Game-header.tsx
@@ -1,15 +1,48 @@
-export const GameHeader = () => {
-  return (
-  <div className="w-full flex items-center justify-between text-base font-bold capitalize text-white">
-    <div>
-      pontuação: 
-      <span className="text-indigo-500 pl-1">0</span>
-    </div>
+import { useEffect, useState } from "react"
+import { useKeyContext } from "../context/Key-provider"
+import { Modal } from "./Modal"
 
-    <div>
-      tentativas restantes: 
-      <span className="text-indigo-500 pl-1">6</span>
-    </div>
-  </div>
+interface GameHeaderProps {
+  isError: boolean
+}
+
+export const GameHeader = ({ isError }: GameHeaderProps) => {
+
+  const { clickedKey } = useKeyContext()
+  const [count, setCount] = useState(7)
+  const [showModal, setShowModal] = useState(false)
+  
+  const handleRemainingAttempts = () => {
+    if (!isError) {
+      setCount(count - 1)
+    }
+    if (count <= 1) {
+      setShowModal(true)
+    }
+  }
+  useEffect(() => {
+    handleRemainingAttempts()
+  }, [clickedKey, isError])
+
+  return (
+    <>
+      {showModal ? (
+        <Modal modalType={true}/>
+      ) : (
+        <></>
+      )}
+      <div className="w-full flex items-center justify-between text-base font-bold capitalize text-white">
+        <div>
+          pontuação:
+          <span className="text-indigo-500 pl-1">0</span>
+        </div>
+
+        <div>
+          tentativas restantes:
+          <span className="text-indigo-500 pl-1">{count}</span>
+        </div>
+      </div>
+    </>
+
   )
 }
