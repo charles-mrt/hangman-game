9ff71354e377252906365c5a1cddd1847d9d8422
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-30 10:27:27 -0300

    feat: Implement Lag component for hangman figure

--- src/app/components/gallows/hangman-figure/Leg.tsx
+++ src/app/components/gallows/hangman-figure/Leg.tsx
@@ -1,5 +1,9 @@
-export const Leg = () => {
+interface LegProps {
+  is_border_color?: boolean
+}
+
+export const Leg = ({ is_border_color }: LegProps) => {
   return (
-    <div className="w-2 h-11  mt-1 rounded-md border-2 border-indigo-800" />
+    <div className={`w-2 h-11  mt-1 rounded-md border-2 ${is_border_color ? 'border-red-500' : 'border-gray-800'}`} />
   )
 }
