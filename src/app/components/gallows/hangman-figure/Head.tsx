77de7b5dd5c393f55d7ea5fd493d83d1271e2739
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-30 10:27:14 -0300

    feat: Implement Head component for hangman figure

--- src/app/components/gallows/hangman-figure/Head.tsx
+++ src/app/components/gallows/hangman-figure/Head.tsx
@@ -1,5 +1,10 @@
-export const Head = () => {
+interface HeadProps {
+  is_border_color?: boolean
+}
+
+export const Head = ({ is_border_color}: HeadProps) => {
+  
   return (
-    <div className="w-8 h-8 rounded-full border-2 border-indigo-800" />
+    <div className={`w-8 h-8 rounded-full border-2  ${is_border_color  ? 'border-red-500' : 'border-gray-800'}`}/>
   )
 }
