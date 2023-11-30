199b5d62f3df5962bdcbd8bc31e536beeccb9248
charles-mrt <charlesmartins.ads@gmail.com>
2023-11-30 10:27:02 -0300

    feat: Implement Body component for hangman figure

--- src/app/components/gallows/hangman-figure/Body.tsx
+++ src/app/components/gallows/hangman-figure/Body.tsx
@@ -1,5 +1,8 @@
-export const Body = () => {
+interface BodyProps {
+  is_border_color?: boolean
+}
+export const Body = ({ is_border_color }: BodyProps) => {
   return (
-    <div className="w-8 h-10  mt-1 rounded-md border-2 border-indigo-800" />
+    <div className={`w-8 h-10  mt-1 rounded-md border-2 ${is_border_color ? 'border-red-500' : 'border-gray-800'}`} />
   )
 }
