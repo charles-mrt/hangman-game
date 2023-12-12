2162b8da2abc9f9a8c95d7bfb40ac24a085168fa
charles-mrt <charlesmartins.ads@gmail.com>
2023-12-12 16:21:04 -0300

    feat: Add Cedilha (Ç) letter to the KeyBoard component

--- src/app/components/Keyboard.tsx
+++ src/app/components/Keyboard.tsx
@@ -4,7 +4,7 @@ const keyBoardLetters = [
   'a', 'b', 'c', 'd', 'e',
   'f', 'g', 'h', 'i', 'j',
   'k', 'l', 'm', 'n', 'o',
-  'p', 'q', 'r', 's', 't',
+  'p', 'q', 'r', 's', 't', 'ç',
   'u', 'v', 'w', 'x', 'y', 'z'
 ]
 
