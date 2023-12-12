f38a2b69b8d654953745217af2934bf7aec269f4
charles-mrt <charlesmartins.ads@gmail.com>
2023-12-12 16:14:27 -0300

    feat: Add styling and functionality to HighlightedLetter component

--- src/app/components/word/Highlighted-letter.tsx
+++ src/app/components/word/Highlighted-letter.tsx
@@ -1,34 +1,28 @@
+import { useKeyContext } from "@/app/context/Key-provider"
 
 interface HighlightedLetterProps {
   letterExist: boolean
-  letterSelected: string
 }
 
-export const HighlightedLetter = ({ letterExist, letterSelected }: HighlightedLetterProps) => {
- 
+export const HighlightedLetter = ({ letterExist }: HighlightedLetterProps) => {
+  const { clickedKey } = useKeyContext()
   return (
-  <>
-    {letterSelected === '' ? (
-      <></>
-    ) : (
-
-    <div className={`
-     ${!letterExist ? 'bg-red-500' : 'bg-green-400'}
-     right-1
-     w-12 h-12  
-     rounded-md
-     text-white 
-     text-2xl 
-     uppercase 
-     font-bold 
-     flex 
-     items-center 
-     justify-center 
-     transition-all`}
-      >
-        {letterSelected}
-      </div>
-    )}
-  </>
+    
+        <div className={`
+          ${!letterExist ? 'bg-red-500' : 'bg-green-400'}
+          right-1
+          w-12 h-12  
+          rounded-md
+          text-white 
+          text-2xl 
+          uppercase 
+          font-bold 
+          flex 
+          items-center 
+          justify-center 
+          transition-all`}
+        >
+          {clickedKey}
+        </div>
   )
 }
