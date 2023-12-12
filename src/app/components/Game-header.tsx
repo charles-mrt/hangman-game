172de8e2ffc2ce5e03799be20455eec08959d6a2
charles-mrt <charlesmartins.ads@gmail.com>
2023-12-12 16:19:46 -0300

    refactor: Simplify and improve code structure in GameHeader component

--- src/app/components/Game-header.tsx
+++ src/app/components/Game-header.tsx
@@ -1,48 +1,20 @@
-import { useEffect, useState } from "react"
-import { useKeyContext } from "../context/Key-provider"
-import { Modal } from "./Modal"
 
 interface GameHeaderProps {
-  isError: boolean
+  life: number
 }
-
-export const GameHeader = ({ isError }: GameHeaderProps) => {
-
-  const { clickedKey } = useKeyContext()
-  const [count, setCount] = useState(7)
-  const [showModal, setShowModal] = useState(false)
-  
-  const handleRemainingAttempts = () => {
-    if (!isError) {
-      setCount(count - 1)
-    }
-    if (count <= 1) {
-      setShowModal(true)
-    }
-  }
-  useEffect(() => {
-    handleRemainingAttempts()
-  }, [clickedKey, isError])
+export const GameHeader = ({ life }: GameHeaderProps) => {
 
   return (
-    <>
-      {showModal ? (
-        <Modal modalType={true}/>
-      ) : (
-        <></>
-      )}
-      <div className="w-full flex items-center justify-between text-base font-bold capitalize text-white">
-        <div>
-          pontuação:
-          <span className="text-indigo-500 pl-1">0</span>
-        </div>
-
-        <div>
-          tentativas restantes:
-          <span className="text-indigo-500 pl-1">{count}</span>
-        </div>
+    <div className="w-full flex items-center justify-between text-base font-bold capitalize text-white">
+      <div>
+        pontuação:
+        <span className="text-indigo-500 pl-1">0</span>
       </div>
-    </>
 
+      <div>
+        tentativas restantes:
+        <span className="text-indigo-500 pl-1">{life}</span>
+      </div>
+    </div>
   )
 }
