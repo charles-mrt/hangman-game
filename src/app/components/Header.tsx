025761d6cd6e880f0b35587ec9b202592f5daf33
charles-mrt <charlesmartins.ads@gmail.com>
2023-12-12 16:11:59 -0300

    refactor: Improve structure and styling in Header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,24 @@
-import { RefreshCw } from "lucide-react"
 
+import { Menu, RefreshCw } from "lucide-react"
 
-export const Header = () => {
+interface HeaderProps {
+  onResetButton: () => void
+}
+
+export const Header = ({ onResetButton }: HeaderProps) => {
   return (
-    <div className="w-full h-14 bg-slate-950 border-b-2 border-indigo-500 flex items-center justify-between px-4">
+    <div className="w-full bg-slate-950 border-b-2 border-indigo-500 flex items-center justify-between px-4 py-2">
+      
+      <Menu className="text-white hover:text-indigo-500"/>
       <h1 className="text-2xl font-bold text-indigo-500 capitalize">
         hang man game
       </h1>
-      <button className="text-white hover:text-indigo-500"><RefreshCw /></button>
+      <button
+        onClick={onResetButton}
+        className="text-white hover:text-indigo-500"
+      >
+        <RefreshCw />
+      </button>
     </div>
-  )
-}
+  );
+};
