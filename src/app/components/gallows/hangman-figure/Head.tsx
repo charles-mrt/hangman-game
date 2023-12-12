09d2bfeb06f7461d3cb1053a76eb53f62241d807
charles-mrt <charlesmartins.ads@gmail.com>
2023-12-12 16:22:01 -0300

    refactor: Improve Head component with optional border color

--- src/app/components/gallows/hangman-figure/Head.tsx
+++ src/app/components/gallows/hangman-figure/Head.tsx
@@ -2,9 +2,8 @@ interface HeadProps {
   is_border_color?: boolean
 }
 
-export const Head = ({ is_border_color}: HeadProps) => {
-  
+export const Head = ({ is_border_color }: HeadProps) => {
   return (
-    <div className={`w-8 h-8 rounded-full border-2  ${is_border_color  ? 'border-red-500' : 'border-gray-800'}`}/>
+    <div className={`w-8 h-8 rounded-full border-2 ${is_border_color  ? 'border-red-500' : 'border-gray-800'}`} />
   )
 }
