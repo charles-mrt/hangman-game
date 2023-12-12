49bb621e1608824d065e99c43866d8836ad65ab4
charles-mrt <charlesmartins.ads@gmail.com>
2023-12-12 16:09:37 -0300

    feat: handle with game audio

---
+++ src/app/use-audio.ts
@@ -0,0 +1,42 @@
+
+
+const gameAudio = {
+  player_lose: '/assets/audio/player-lose.mp3',
+  player_win: '/assets/audio/player-win.mp3',
+  wrong_letter: '/assets/audio/wrong-letter.mp3',
+  correct_letter: '/assets/audio/correct-letter.mp3',
+  click: '/assets/audio/click.mp3',
+}
+
+export const useAudio = () => {
+  const playAudio = (audio:string) => {
+    new Audio(audio).play()
+  }
+
+  const playerLose = () => {
+    playAudio(gameAudio.player_lose)
+  }
+  const playerWin = () => {
+    playAudio(gameAudio.player_win)
+  }
+
+  const wrongLetter = () => {
+    playAudio(gameAudio.wrong_letter)
+  }
+
+  const correctLetter = () => {
+    playAudio(gameAudio.correct_letter)
+  }
+
+  const keyboardClick = () => {
+    playAudio(gameAudio.click)
+  }
+
+  return {
+    playerLose,
+    playerWin,
+    wrongLetter,
+    correctLetter,
+    keyboardClick
+  }
+}
