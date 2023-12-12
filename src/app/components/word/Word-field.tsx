7c529013eb6f2d6888dea3133adb96e00de42620
charles-mrt <charlesmartins.ads@gmail.com>
2023-12-12 16:19:20 -0300

    refactor: Enhance game logic and structure in WordField component

--- src/app/components/word/Word-field.tsx
+++ src/app/components/word/Word-field.tsx
@@ -1,47 +1,130 @@
 'use client'
+
 import { useState, useEffect } from 'react'
-import { useKeyContext } from "@/app/context/Key-provider"
 
-interface WordFieldProps {
-  totalField: number
-}
-const word = "caminhar"
+import { useKeyContext } from '@/app/context/Key-provider'
+import { usePlayerLives } from './hooks/use-player-lives'
+import { useRandomWord } from './hooks/use-to-chose-word'
 
-export const WordField = ({ totalField }: WordFieldProps) => {
-  const wordLetters = word.split("")
+import { HighlightedLetter } from './Highlighted-letter'
+import { GameHeader } from '../Game-header'
+import { GallowsPole } from '../gallows/Gallows-pole'
+import { Header } from '../Header'
+import { Modal } from '../Modal'
+
+
+export const WordField = () => {
+
+  const getRandomWord = useRandomWord()
+
   const { clickedKey } = useKeyContext()
+
+  const [word, setWord] = useState<string>('')
+  const wordLetters = word.split("")
+  const totalField = wordLetters.length
+
   const [revealedLetters, setRevealedLetters] = useState<string[]>([])
+  const [isLetterExist, setIsLetterExist] = useState(true)
+  const [isPlayerWins, setIsPlayerWins] = useState(false)
+  const [clearHighLighted, setClearHightLighted] = useState(true)
+
+  const { handleLetterError, handleResetLife, lives } = usePlayerLives()
+
+  const chooseRandomLetters = () => {
+    if (totalField >= 6 && revealedLetters.length === 0) {
+      const randomIndexes: number[] = Array.from({ length: 3 }, () => Math.floor(Math.random() * totalField))
+      setRevealedLetters(randomIndexes.map(index => wordLetters[index]))
+    }
+  }
+
+  const handleKeyClick = () => {
+    switch (true) {
+      case clickedKey && wordLetters.includes(clickedKey) && !revealedLetters.includes(clickedKey):
+        setRevealedLetters((prevLetters) => {
+
+          const occurrencesInWord = wordLetters.filter(letter => letter === clickedKey)
+          const updatedLetters = [...prevLetters, ...occurrencesInWord]
+
+          if (wordLetters.every((letter) => updatedLetters.includes(letter))) {
+            setIsPlayerWins(true)
+          }
+
+          return updatedLetters
+        })
+        setIsLetterExist(true)
+        setClearHightLighted(false)
+        break
+
+      case clickedKey && !wordLetters.includes(clickedKey):
+        setIsLetterExist(false)
+        handleLetterError()
+        setClearHightLighted(false)
+        break
+
+      default:
+        break
+    }
+  }
 
   useEffect(() => {
-  
-    if (wordLetters.includes(clickedKey) && !revealedLetters.includes(clickedKey)) {
-      setRevealedLetters((prevLetters) => [...prevLetters, clickedKey])
-    }
-  }, [clickedKey, revealedLetters, wordLetters])
+    setWord(getRandomWord())
+    chooseRandomLetters()
+  }, [])
+
+  useEffect(() => {
+    handleKeyClick()
+  }, [clickedKey])
+  console.log(word)
+
+  const handleRestartGame = () => {
+    handleResetLife()
+    setIsPlayerWins(false)
+    setRevealedLetters([])
+    setWord(getRandomWord())
+    setClearHightLighted(true)
+  }
 
   return (
-    <div className="flex flex-wrap gap-2 justify-center">
+    <>
+      <Modal life={lives} onResetButton={handleRestartGame} isPlayerWins={isPlayerWins} />
+      <Header onResetButton={handleRestartGame} />
 
-      {wordLetters.map((letter, index) => (
+      <div className="h-full p-5 flex flex-col gap-6 justify-between">
 
-        <div className="flex flex-col items-center justify-end" key={index}>
+        <div className="w-full flex flex-col justify-between gap-10">
 
-          {revealedLetters.includes(letter) ? (
-            <>
-              <span className="text-2xl leading-6 font-bold uppercase text-white">
-                {letter}
-              </span>
-              <div className="w-8 h-[3px] bg-indigo-500" />
-            </>
-          ) : (
-            <>
-              <div className="w-8 h-[3px] bg-indigo-500" />
-            </>
-          )}
+          <GameHeader life={lives} />
+
+          <div className="flex items-start justify-between">
+            <GallowsPole life={lives} playerWins={isPlayerWins} />
+            {!clearHighLighted && <HighlightedLetter letterExist={isLetterExist} />}
+          </div>
+
+          <div className="flex flex-wrap gap-2 justify-center">
+
+            {wordLetters.map((letter, index) => (
+              <div className="flex flex-col items-center justify-end" key={index}>
+                {revealedLetters.includes(letter) ? (
+                  <>
+                    <span className="text-2xl leading-6 font-bold uppercase text-white">
+                      {letter}
+                    </span>
+                    <div className="w-8 h-[3px] bg-indigo-500" />
+                  </>
+                ) : (
+                  <>
+                    <div className="h-8" />
+                    <div className="w-8 h-[3px] bg-indigo-500" key={index} />
+                  </>
+                )}
+              </div>
+            ))}
+
+          </div>
 
         </div>
-      ))}
-      
-    </div>
+
+      </div>
+    </>
   )
 }
