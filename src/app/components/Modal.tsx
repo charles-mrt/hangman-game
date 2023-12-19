2f89c2110aafe5387006a74695d9f74da1948dfb
charles-mrt <charlesmartins.ads@gmail.com>
2023-12-19 11:02:30 -0300

    refactor: Improve code structure and readability

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -1,5 +1,4 @@
 
-import { useState } from "react"
 
 interface ModalProps {
   word: string
@@ -9,58 +8,54 @@ interface ModalProps {
 }
 
 export const Modal = ({ word, life, onResetButton, isPlayerWins }: ModalProps) => {
-
-  const [showModal, setShowModal] = useState(true)
-
-  const handleCloseModal = () => {
-    onResetButton()
-    setShowModal(false)
-    resetModalState()
-  }
-
-  const resetModalState = () => {
-    if (life === 0 || isPlayerWins) setShowModal(true)
-  }
+  const handleCloseModal = () => onResetButton()
 
   return (
-    <>
-      {showModal && (isPlayerWins || life !== 0) && (
-        
-        <div className=" fixed z-50 left-0 top-0 w-screen h-screen ">
+    <div className="modal-animation fixed z-50 left-0 top-0 w-screen h-screen overflow-hidden">
 
-          <div className={`
-              ${isPlayerWins ? 'bg-green-500' : 'bg-red-500'} 
-              bg-opacity-40  
-              w-full 
-              h-full  
-              flex              
-              items-center 
-              justify-center                                                       
-            `}>
+      {isPlayerWins &&
+        <div className="bg-green-500 bg-opacity-40 w-full h-full flex items-center justify-center" >
 
-            <div className=" w-11/12 bg-amber-100 rounded-md border-4 border-gray-400 p-4 flex flex-col items-center justify-evenly gap-5 shadow-2xl">
+          <div className=" w-auto bg-amber-100 rounded-md border-4 border-gray-400 p-4 flex flex-col items-center justify-evenly gap-5 shadow-2xl">
             <span className="text-gray-500 text-2xl font-bold text-center">
-              resposta: <span className="underline">{word}</span>
+              resposta: <span className="text-green-600">{word}</span>
             </span>
-              <span className="text-gray-500 text-2xl font-bold text-center">
-                {
-                  isPlayerWins
-                    ? "Parabéns, você venceu!"
-                    : "Você perdeu, tente novamente!"
-                }
-              </span>
+            <span className="text-gray-500 text-2xl font-bold text-center">
+              Parabéns, você venceu!
+            </span>
+            <button
+              className="bg-indigo-500 text-white p-4 text-2xl font-bold rounded-md"
+              onClick={handleCloseModal}
+            >
+              Jogar novamente
+            </button>
 
-              <button
-                className="bg-indigo-500 text-white p-4 text-2xl font-bold rounded-md"
-                onClick={handleCloseModal}
-              >
-                Jogar novamente
-              </button>
-
-            </div>
           </div>
         </div>
-      )}
-    </>
+      }
+
+      {life === 0 &&
+
+        <div className="bg-red-500 bg-opacity-40 w-full h-full flex items-center justify-center " >
+
+          <div className=" w-11/12 bg-amber-100 rounded-md border-4 border-gray-400 p-4 flex flex-col items-center justify-evenly gap-5 shadow-2xl">
+            <span className="text-gray-500 text-2xl font-bold text-center">
+              resposta: <span className="text-red-600">{word}</span>
+            </span>
+            <span className="text-gray-500 text-2xl font-bold text-center">
+              Você perdeu, tente novamente!
+            </span>
+            <button
+              className="bg-indigo-500 text-white p-4 text-2xl font-bold rounded-md"
+              onClick={handleCloseModal}
+            >
+              Jogar novamente
+            </button>
+
+          </div>
+        </div>
+      }
+
+    </div>
   )
 }
