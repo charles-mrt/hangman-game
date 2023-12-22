efb24dd5de35d0d2bcc9cabab57ee747c70a881b
charles-mrt <charlesmartins.ads@gmail.com>
2023-12-22 13:59:03 -0300

    style: fix position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ import { Footer } from './components/Footer'
 export default function Home() {
   return (
     <main className="w-screen h-screen">
-      <div className="max-w-[430px] max-h-[932px] w-full h-full bg-gray-950 flex flex-col justify-between">
+      <div className="max-w-[430px] max-h-[932px] w-full h-full bg-gray-950 flex flex-col justify-between items-center">
         <KeyProvider>
           <WordField />
           <KeyBoard />
