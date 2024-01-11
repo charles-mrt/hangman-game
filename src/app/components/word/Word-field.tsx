1414b7f9f59fe8b33f9cb96716ed12a5ceb4a069
charles-mrt <charlesmartins.ads@gmail.com>
2024-01-11 13:39:20 -0300

    style: fix header space

--- src/app/components/word/Word-field.tsx
+++ src/app/components/word/Word-field.tsx
@@ -95,7 +95,7 @@ export const WordField = () => {
       }
       <Header onResetButton={handleRestartGame} />
 
-      <div className="h-full p-5 flex flex-col gap-6 justify-between">
+      <div className="h-full w-full p-5 flex flex-col gap-6 justify-between">
 
         <div className="w-full flex flex-col justify-between gap-10">
 
